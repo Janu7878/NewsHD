c8b0fbcd7a011e5e1fb0cda59cdac495b1a68cf4
Janu7878 <119414805+Janu7878@users.noreply.github.com>
2022-12-25 07:48:45 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/',
+        source: '/about',
         destination: 'https://sx.forty-shades.com',
         permanent: false,
         basePath: false

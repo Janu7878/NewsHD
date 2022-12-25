e270d82cfa8a3cf45b5c5fd6ba32c3e6032f449c
Janu7878 <119414805+Janu7878@users.noreply.github.com>
2022-12-25 08:57:55 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: '/',
+        destination: 'https://www.google.com',
         permanent: false,
         basePath: false
       },
